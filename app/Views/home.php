b63a7b94ae90f400ab8e0047994d18ac2eb294c6
Varun Sharma <varunk22110@gmail.com>
2021-04-06 21:08:23 +0530

    Changed banner images on Home page.

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,9 +1,50 @@
-<div id="exampleSlider">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1" class=""></li>
+    <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/banner/banner_1.jpg" alt="Banner 1">
+    </div>
+
+    <div class="item">
+      <img src="images/banner/banner_2.jpg" alt="Banner 2">
+    </div>
+
+    <div class="item">
+      <img src="images/banner/banner_3.jpg" alt="Banner 5">
+    </div>
+
+    <div class="item">
+      <img src="images/banner/banner_4.jpg" alt="Banner 4">
+    </div>
+
+    <div class="item">
+      <img src="images/banner/banner_5.jpg" alt="Banner 6">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
+<!-- <div id="exampleSlider">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+</div> -->
 
 <!-- banner-bottom -->	
 <div class="banner-bottom">
